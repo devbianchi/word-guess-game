fae76a620ab423e193c735c2ffcb9facde2d1f4a
Guilherme Machado <guilherme.bianchi.machado@gmail.com>
2026-07-11 01:16:00 -0300

    Fix: no images to social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ export function Footer() {
       <p>&copy; 2026 Word Guess! All rights reserved. By: Guilherme Machado</p>
       <section className="social-media flex justify-center items-center mt-6 gap-6">
         <a href="https://github.com/devbianchi" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
-          <img src="../assets/github.png" alt="GitHub" className="w-6 h-6" /> <h1>GitHub</h1>
+          <h1>GitHub</h1>
         </a>
         <a href="https://www.linkedin.com/in/guilherme-bmachado/?locale=en" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
-          <img src="../assets/linkedin.png" alt="LinkedIn" className="w-6 h-6" /> <h1>LinkedIn</h1>
+          <h1>LinkedIn</h1>
         </a>
       </section>
     </footer>
